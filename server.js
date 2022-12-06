0867d77651364192b17e581fcd3dcc7ca3f4dcbd
K-156 <limstephanie156@gmail.com>
2022-12-06 08:46:36 +0800

    connect backend with mongodb

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 import dotenv from "dotenv";
 dontenv.config();
 
+// db and authenticate user
+import connectDB from "./db/connect.js";
+
 // middleware
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
@@ -17,6 +20,11 @@ app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
+  } catch (error) {}
+};
