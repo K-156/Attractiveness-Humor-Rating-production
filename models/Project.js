cb5f60fffcb42712c731e26fdb246690ece0ea00
K-156 <limstephanie156@gmail.com>
2022-12-09 08:07:58 +0800

    create project schema

--- models/Project.js
+++ models/Project.js
@@ -1,23 +1,36 @@
 import mongoose from "mongoose";
 
-const ProjectSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: [true, "Please provide name"],
+const ProjectSchema = new mongoose.Schema({
+  // name: {
+  //   type: String,
+  //   required: [true, "Please provide name"],
+  // },
+  // description: {
+  //   type: String,
+  //   required: [true, "Please provide description"],
+  // },
+  // attractivenessInstruc: {
+  //   type: String,
+  //   required: [true, "Please provide attractiveness instructions"],
+  // },
+  // audioInstruc: {
+  //   type: String,
+  //   enum: ["withdrawal", "deposit"],
+  //   default: "withdrawal",
+  // },
+  proj: [
+    {
+      name: String,
+      description: String,
+      img: Image,
+      attributes: [
+        {
+          field: String,
+          value: String,
+        },
+      ],
     },
-    description: {
-      type: String,
-      required: [true, "Please provide description"],
-    },
-    attractivenessInstruc: {
-      type: String,
-      required: [true, "Please provide attractiveness instructions"],
-    },
-    audioInstruc: {
-      type: String,
-      enum: ["withdrawal", "deposit"],
-      default: "withdrawal",
-    },
-  },
-);
+  ],
+});
+
+export default mongoose.model("Projects", ProjectSchema);
