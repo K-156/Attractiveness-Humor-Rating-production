127bf3f270907cb8dec5573f8c0e8b84e9012fea
K-156 <limstephanie156@gmail.com>
2022-12-19 21:28:57 +0800

    Update NavBar.js

--- client/src/Components/NavBar/NavBar.js
+++ client/src/Components/NavBar/NavBar.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 import { Box, Button, Typography } from "@mui/material";
 import { MdSpaceDashboard } from "react-icons/md";
@@ -9,51 +9,61 @@ import { FiLogOut } from "react-icons/fi";
 import "./NavBar.css";
 
 const NavBar = () => {
+  const navigate = useNavigate();
+  const location = useLocation()
 
-    const navigate = useNavigate();
-    const [tab, setTab] = useState("overview");
+  const [tab, setTab] = useState(location.pathname.slice(1));
 
-    const handleOnClick = (route) => {
-        // navigate(`/${route}`);
-        // setTab(route)
-    }
+  const handleOnClick = (route) => {
+    navigate(`/${route}`);
+    setTab(route)
+  };
 
-    return (
+  return (
     <>
-    <div id="NavBar">
+      <div id="NavBar">
         <Box mt={12}>
-            <Button 
-                className="navBarButton"
-                sx={{background: tab === "overview" ? "#1d353f" : "#264653"}}
-                onClick={handleOnClick("overview")}
-            >
-                <MdSpaceDashboard size={25} style={{margin:"15px"}}/>
-                <Typography variant="subtitle1" pl={1}>Overview</Typography>
-            </Button>
-            <Button 
-                className="navBarButton"
-                sx={{background: tab === "project" ? "#1d353f" : "#264653"}}
-                onClick={handleOnClick("project")}
-            >
-                <BsCardChecklist size={25} style={{margin:"15px"}}/>
-                <Typography variant="subtitle1" pl={1}>Project</Typography>
-            </Button>
+          <Button
+            className="navBarButton"
+            sx={{ background: tab === "overview" ? "#1d353f" : "#264653" }}
+            onClick={() => handleOnClick("overview")}
+          >
+            <MdSpaceDashboard size={25} style={{ margin: "15px" }} />
+            <Typography variant="subtitle1" pl={1}>
+              Overview
+            </Typography>
+          </Button>
+          <Button
+            className="navBarButton"
+            sx={{ background: tab === "projects" ? "#1d353f" : "#264653" }}
+            onClick={() => handleOnClick("projects")}
+          >
+            <BsCardChecklist size={25} style={{ margin: "15px" }} />
+            <Typography variant="subtitle1" pl={1}>
+              Project
+            </Typography>
+          </Button>
         </Box>
 
         <Box className="logoutPosition">
-            <Button 
-                variant="contained"
-                className="logoutButton"
-                sx={{mx:7, px: 3, backgroundColor:"#FFFFFF", '&:hover': {backgroundColor: "#C59D5F"}}}
-                onClick={() => navigate("/")}
-            >
-                <FiLogOut size={20} style={{margin:"10px 10px 10px 0px"}}/>
-                <Typography variant="subtitle1">Logout</Typography>
-            </Button>
+          <Button
+            variant="contained"
+            className="logoutButton"
+            sx={{
+              mx: 7,
+              px: 3,
+              backgroundColor: "#FFFFFF",
+              "&:hover": { backgroundColor: "#C59D5F" },
+            }}
+            onClick={() => navigate("/")}
+          >
+            <FiLogOut size={20} style={{ margin: "10px 10px 10px 0px" }} />
+            <Typography variant="subtitle1">Logout</Typography>
+          </Button>
         </Box>
-    </div>
+      </div>
     </>
-    )  
-}
+  );
+};
 
 export default NavBar;
