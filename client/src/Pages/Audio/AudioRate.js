17b2ab6484d3a10bb3851f49a57ee58e3d4cd70d
K-156 <limstephanie156@gmail.com>
2022-12-21 22:19:38 +0800

    Update AudioRate.js

--- client/src/Pages/Audio/AudioRate.js
+++ client/src/Pages/Audio/AudioRate.js
@@ -11,13 +11,16 @@ import { isValid } from "../../Utils/isValid";
 import IntroMessage from "../../Components/Message/IntroMessage";
 import Instruction from "../../Components/Instruction/Instruction";
 import Audio from "../../Components/Audio/Audio";
+import links from "../../Utils/links";
 
 const AudioRate = ({ title, link, isWritten }) => {
-  const { updateUser } = useAppContext();
+  const { updateUser, sectionNum, nextSection } = useAppContext();
   const [rating, setRating] = useState({});
   const navigate = useNavigate();
 
-  const data = JSON.parse(localStorage.getItem("data"));
+  const { data, sections } = JSON.parse(localStorage.getItem("data"));
+  const { path } = links.find((link) => link.id === sections[sectionNum + 1]);
+
   const user = JSON.parse(localStorage.getItem("user"));
   const firstCandidate = user.userResponse.rank[0];
   const lastCandidate =
@@ -49,9 +52,9 @@ const AudioRate = ({ title, link, isWritten }) => {
           },
           id: user._id,
         });
-    navigate(link);
+    nextSection();
+    navigate(path);
   };
-
 
   return (
     <div>
@@ -66,14 +69,14 @@ const AudioRate = ({ title, link, isWritten }) => {
       <Grid container className="centerPadding" gap={2}>
         <Grid item xs={4} px={4}>
           <Box className="imageBox">
-            <img
+            {/* <img
               src={
                 link.includes("q2")
                   ? data.proj[firstCandidate].img
                   : data.proj[lastCandidate].img
               }
               alt="candidate"
-            />
+            /> */}
           </Box>
           {isWritten ? (
             <IntroMessage text={data.writtenIntro} />
@@ -83,28 +86,25 @@ const AudioRate = ({ title, link, isWritten }) => {
         </Grid>
         <Grid item xs={7} px={4}>
           <AudioForm
-            ques={data.audioRatingInstruc}
+            ques={data[sectionNum][sections[sectionNum]].questions}
             setRating={setRating}
             isWritten={isWritten}
           />
         </Grid>
-        <Grid
-          item
-          xs={12}
-          className="spaceBetween"
-          sx={{py: 3, px: 9}}
-        >
+        <Grid item xs={12} className="spaceBetween" sx={{ py: 3, px: 9 }}>
           {parseInt(title) === 1 ? (
-            <PrevButton 
-              isSurvey={true}
-              link="/audio-instruction" 
-            />
+            <PrevButton isSurvey={true} link="/audio-instruction" />
           ) : (
             <Box></Box>
           )}
           <NextButton
             isSurvey={true}
-            disabled={!isValid(rating, Object.keys(data.audioRatingInstruc).length)}
+            disabled={
+              !isValid(
+                rating,
+                data[sectionNum][sections[sectionNum]].questions.length
+              )
+            }
             handleOnSubmit={handleOnSubmit}
           />
         </Grid>
