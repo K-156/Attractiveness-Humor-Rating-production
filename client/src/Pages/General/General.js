d6d18c1c73f0613b4559a97ae304cb08d5a4af58
K-156 <limstephanie156@gmail.com>
2022-12-21 23:14:12 +0800

    Update General.js

--- client/src/Pages/General/General.js
+++ client/src/Pages/General/General.js
@@ -1,64 +1,68 @@
 import { useAppContext } from "../../Context/AppContext";
 import { useEffect } from "react";
 
-import { 
-    Box, 
-    Card, 
-    CardContent, 
-    Typography
-} from "@mui/material";
-
+import { Box, Card, CardContent, Typography } from "@mui/material";
+import NextButton from "../../Components/NavButton/NextButton";
 import { colorPalette } from "../../Utils/colorPalette";
+import links from "../../Utils/links";
 
-const text = "Thank you for completing the survey.\nYour responses have been submitted.\n\nHave a nice day!"
+const text =
+  "Thank you for completing the survey.\nYour responses have been submitted.\n\nHave a nice day!";
 
 const General = () => {
+  const { removeUserFromLocalStorage, theme, sectionNum } = useAppContext();
 
-    const {removeUserFromLocalStorage, theme } = useAppContext();
+  const { data, sections } = JSON.parse(localStorage.getItem("data"));
+  let { path } = "/"
 
-    useEffect(() => {
-        removeUserFromLocalStorage()
-    },[])
-    sessionStorage.clear();
+  if (sectionNum !== sections.length) {
+    path = links.find((link) => link.id === sections[sectionNum + 1]);
+  }
 
-    return (
-    <div 
-        className={`backgroundImage-${theme}`}
-        style={{backgroundColor: colorPalette[theme]["background"]}}
+  //   setTimeout(() => {
+  //     removeUserFromLocalStorage();
+  //   }, 300);
+  //   sessionStorage.clear();
+
+  return (
+    <div
+      className={`backgroundImage-${theme}`}
+      style={{ backgroundColor: colorPalette[theme]["background"] }}
     >
-        <script>
-            {document.title="Thank you"}
-        </script>
-        <Box className="center">
-            <img
-                src={require(`../../Assets/Theme/${theme}/general.svg`)}
-                style={{
-                    width: "20%",
-                    height: "100%",
-                    margin: "50px 20px 20px 20px"
-                }}
-            />
-        </Box>       
-        <Box className="center">
-            <Card 
-                sx={{
-                    background: colorPalette[theme]["primary"], 
-                    color:"#FFFFFF", 
-                    minWidth:"500px",
-                }} 
-            >
-                <CardContent sx={{p:"24px"}}>
-                    <Typography
-                        className="textCenter"
-                        sx={{whiteSpace:"pre-line"}}
-                    >
-                        {text}
-                    </Typography>
-                </CardContent>
-            </Card>
+      <script>{(document.title = "Thank you")}</script>
+      <Box className="center">
+        <img
+          src={require(`../../Assets/Theme/${theme}/general.svg`)}
+          style={{
+            width: "20%",
+            height: "100%",
+            margin: "50px 20px 20px 20px",
+          }}
+          alt="complete"
+        />
+      </Box>
+      <Box className="center">
+        <Card
+          sx={{
+            background: colorPalette[theme]["primary"],
+            color: "#FFFFFF",
+            minWidth: "500px",
+          }}
+        >
+          <CardContent sx={{ p: "24px" }}>
+            <Typography className="textCenter" sx={{ whiteSpace: "pre-line" }}>
+              {data[sectionNum][sections[sectionNum]].text}
+            </Typography>
+          </CardContent>
+        </Card>
+      </Box>
+      {data[sectionNum][sections[sectionNum]].isNext === "true" && (
+        <Box className="flexEnd" sx={{ py: 3, width: "80%", px:6 }}>
+          <NextButton isSurvey={true} link={path} />
         </Box>
+      )}
     </div>
-    )
-}
+  );
+};
 
 export default General;
