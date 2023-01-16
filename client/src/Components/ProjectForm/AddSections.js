211a930fe114640839653f923b20edd01bd133b7
tangihan <78784964+tangihan@users.noreply.github.com>
2023-01-17 00:53:06 +0800

    added error msg for add sections

--- client/src/Components/ProjectForm/AddSections.js
+++ client/src/Components/ProjectForm/AddSections.js
@@ -1,8 +1,8 @@
-import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react"
+import { Link } from "react-router-dom";
 
 import {
   Box,
-  Button,
   Card,
   CardContent,
   FormControl,
@@ -17,16 +17,46 @@ import "./ProjectForm.css";
 import ReorderList from "../ReorderList/ReorderList";
 
 const AddSection = ({ formData, setFormData }) => {
-  const navigate = useNavigate();
+
+  const [error, setError] = useState({
+    profile: false, rank: false
+  })
 
   const onDelete = (index) => {
+
+    const value = formData[index];  
+    if (value === 1 || value === 3) {
+      const findNext = formData.indexOf(value, index + 1) 
+      const valueList = value === 1 ? [2,3,4,5,6] : [4,5,6];
+      const nextValue = formData.slice(index + 1, findNext === -1 ? formData.length : findNext);
+      const filteredArray = nextValue.filter(value => valueList.includes(value));
+      const beforeValues = formData.slice(0, index)
+      if (filteredArray.length > 0 && !beforeValues.includes(value)) {
+        setError({profile: value === 1, rank: value === 3 });
+            return;
+      }
+    }
+   
     if (index === 0) {
       formData.shift();
     } else {
       formData.splice(index, 1);
     }
     setFormData([...formData]);
+    setError({rank: false, profile: false});
   };
+
+  const handleAddSection = (event) => {
+    const value = event.target.value;
+    if (value >= 2 && value <= 6 && !formData.includes(1)) {
+      setError({profile: true, rank: false})
+    } else if (value >= 4 && value <= 6 && !formData.includes(3)) {
+      setError({profile:false, rank: true})
+    } else {
+      setError({profile: false, rank: false})
+      setFormData(formData.concat(Number(value)));
+    }   
+  }
 
   return (
     <Card>
@@ -57,9 +87,7 @@ const AddSection = ({ formData, setFormData }) => {
                 label="Select template"
                 value=""
                 InputLabelProps={{shrink:false}}
-                onChange={(event) => {
-                  setFormData(formData.concat(Number(event.target.value)));
-                }}
+                onChange={handleAddSection}
               >
                 {_.map(templates, (value, key) => {
                   return (
@@ -77,8 +105,9 @@ const AddSection = ({ formData, setFormData }) => {
                 formData={formData}
                 setFormData={setFormData}
                 onDelete={onDelete}
+                error={error}
+                setError={setError}
               />
-
             </Box>
           </Box>
         </FormControl>
--- client/src/Components/ReorderList/ReorderList.js
+++ client/src/Components/ReorderList/ReorderList.js
@@ -1,32 +1,19 @@
-import { useEffect, useState } from "react";
-
 import { 
+    Alert,
+    AlertTitle, 
     Box, 
     Button, 
     Typography
 } from "@mui/material";
-import _ from "lodash";
+import _, { filter } from "lodash";
 import { TfiArrowCircleUp, TfiArrowCircleDown } from "react-icons/tfi";
 import { RiDeleteBin6Fill } from "react-icons/ri";
 
 import { templates } from "../../Utils/templateList";
 
-const ReorderList = ({ formData, setFormData, onDelete }) => {
+const ReorderList = ({ formData, setFormData, onDelete, error, setError}) => {
 
-    const reorderArray = (event, originalArray) => {
-        const movedItem = originalArray.find((item, index) => index === event.oldIndex);
-        const remainingItems = originalArray.filter((item, index) => index !== event.oldIndex);
-      
-        const reorderedItems = [
-            ...remainingItems.slice(0, event.newIndex),
-            movedItem,
-            ...remainingItems.slice(event.newIndex)
-        ];
-      
-        return reorderedItems;
-      }
-      
-      function changeOrder(index, direction) {
+    const changeOrder = (index, direction) => {
         let newIndex = index;
         if (direction === "up" && index > 0) {  
             newIndex -= 1;
@@ -34,8 +21,43 @@ const ReorderList = ({ formData, setFormData, onDelete }) => {
             newIndex += 1;
         }
         setFormData(reorderArray({oldIndex: index, newIndex: newIndex}, formData));
+    }
+
+    const reorderArray = (event, originalArray) => {
+        const oldIndex = event.oldIndex;
+        const newIndex = event.newIndex;
+        const movedItem = originalArray.find((item, index) => index === oldIndex);
+        const remainingItems = originalArray.filter((item, index) => index !== oldIndex);
+
+        const reorderedItems = [
+            ...remainingItems.slice(0, newIndex),
+            movedItem,
+            ...remainingItems.slice(newIndex)
+        ];
+
+        if (!validArray(reorderedItems, 1) || !validArray(reorderedItems, 3)) {
+            return originalArray
+        }
+
+        return reorderedItems;
       }
-      
+    
+    const validArray = (array, num) => {
+        const index = array.indexOf(num);
+        const beforeValues = array.slice(0, index);
+        const valueList = num === 1 ? [2,3,4,5,6] : [4,5,6];
+        const filteredArray = beforeValues.filter(value => valueList.includes(value));
+        if (filteredArray.length > 0) {
+            setError({
+                profile: num === 1, 
+                rank: num === 3
+            })
+            return false;
+        }
+        
+        setError({ profile: false, rank: false });
+        return true;
+    }  
 
     return(
         <Box 
@@ -83,10 +105,15 @@ const ReorderList = ({ formData, setFormData, onDelete }) => {
                             </Button>
                         </Box>
                     </Box>
-                    
-                )
-            }) 
-            }            
+                )}) 
+            }
+            { (error["profile"] || error["rank"]) &&
+             <Alert severity="error" >
+                <AlertTitle sx={{ fontWeight: "bold" }}>Invalid Section Order</AlertTitle>
+                {error["profile"] && "Profile section (Template 1) must be added before Templates 2 to 6"} 
+                {error["rank"] && "Rank section (Template 3) must be added before Templates 4 to 6"}
+            </Alert>         
+            }   
         </Box>
     )
 }
