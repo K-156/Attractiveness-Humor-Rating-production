d7a13b284c6208cc41feb8f48745450316f8785c
K-156 <limstephanie156@gmail.com>
2023-01-27 16:20:51 +0800

    fix color palette

--- client/src/Layout/GeneralLayout.js
+++ client/src/Layout/GeneralLayout.js
@@ -2,24 +2,22 @@ import { Outlet } from "react-router-dom";
 import { useAppContext } from "../Context/AppContext";
 
 import { Box } from "@mui/material";
-
 import { colorPalette } from "../Utils/colorPalette";
 
 const GeneralLayout = () => {
+  const { theme } = useAppContext();
 
-    const { theme } = useAppContext();
-    
-    return(
-        <Box
-            sx={{
-                backgroundColor: colorPalette[theme]?.background,
-                p: "25px 25px 50px 25px",
-                minHeight:"100vh"
-            }}
-        >
-            <Outlet />
-        </Box>
-    )
-}
+  return (
+    <Box
+      sx={{
+        backgroundColor: colorPalette[theme]?.background,
+        p: "25px 25px 50px 25px",
+        minHeight: "100vh",
+      }}
+    >
+      <Outlet />
+    </Box>
+  );
+};
 
 export default GeneralLayout;
